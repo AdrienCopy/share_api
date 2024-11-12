e1bc10c8c8ba6fcdde782245a8c42f253f6bfd85
Adrien Boels <adrien.boels@gmail.com>
2024-11-12 14:36:03 +0100

    2

--- api/getLinkedInToken.js
+++ api/getLinkedInToken.js
@@ -1,32 +1,41 @@
-const fetch = require('node-fetch');
-
 module.exports = async (req, res) => {
-    const { code } = req.body;
+    // Gérer les requêtes POST
+    if (req.method === 'POST') {
+        // Exemples de gestion CORS
+        res.setHeader('Access-Control-Allow-Origin', '*'); // Permet les requêtes de n'importe quelle origine
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // Spécifie les méthodes autorisées
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Autorise l'en-tête Content-Type
 
-    const clientId = '781r6zese4aq4i';
-    const clientSecret = 'WPL_AP1.j06rT3lNZKNF2tDB.xlKHlA==';
-    const redirectUri = 'https://adriencopy.github.io/share/linkedin.html';
+        const { code } = req.body;
 
-    const url = 'https://www.linkedin.com/oauth/v2/accessToken';
-    const params = new URLSearchParams({
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: redirectUri,
-        client_id: clientId,
-        client_secret: clientSecret
-    });
+        const clientId = '781r6zese4aq4i';
+        const clientSecret = 'WPL_AP1.j06rT3lNZKNF2tDB.xlKHlA==';
+        const redirectUri = 'https://adriencopy.github.io/share/linkedin.html';
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: params
+        const url = 'https://www.linkedin.com/oauth/v2/accessToken';
+        const params = new URLSearchParams({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: redirectUri,
+            client_id: clientId,
+            client_secret: clientSecret
         });
-        const data = await response.json();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: params
+            });
+            const data = await response.json();
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    } else {
+        // Répondre à une méthode non autorisée si la méthode n'est pas POST
+        return res.status(405).json({ error: 'Méthode non autorisée' });
     }
 };
