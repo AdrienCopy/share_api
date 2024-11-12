d612226df88a4a4563c5b237b2432018ac65f94f
Adrien Boels <adrien.boels@gmail.com>
2024-11-12 15:45:18 +0100

    next

--- api/getLinkedInUserId.js
+++ api/getLinkedInUserId.js
@@ -1,11 +1,14 @@
 module.exports = async (req, res) => {
-    // Gestion des requêtes POST
-    if (req.method === 'POST') {
-        // Exemples de gestion CORS
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    if (req.method === 'POST') {
         const { accessToken } = req.body;
 
         try {
@@ -23,14 +26,11 @@ module.exports = async (req, res) => {
             if (data.error) {
                 throw new Error(data.message);
             }
-
-            // Renvoyer l'ID de l'utilisateur
             res.status(200).json({ personId: data.id });
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
     } else {
-        // Méthode non autorisée si autre que POST
         res.status(405).json({ error: 'Méthode non autorisée' });
     }
 };
