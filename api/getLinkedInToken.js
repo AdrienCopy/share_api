d59ad28616f6006cd581619f7f02b2758f7d9ea0
Adrien Boels <adrien.boels@gmail.com>
2024-11-12 14:57:37 +0100

    token

--- api/getLinkedInToken.js
+++ api/getLinkedInToken.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
         const clientSecret = 'WPL_AP1.j06rT3lNZKNF2tDB.xlKHlA==';
         const redirectUri = 'https://adriencopy.github.io/share/linkedin.html';
 
-        const url = 'https://www.linkedin.com/oauth/v2/accessToken';
+        const tokenUrl = 'https://www.linkedin.com/oauth/v2/accessToken';
         const params = new URLSearchParams({
             grant_type: 'authorization_code',
             code: code,
