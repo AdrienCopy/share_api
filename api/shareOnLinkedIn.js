53a303a0e98e9f25ba4d91bcfdf7631b3441f2b0
Adrien Boels <adrien.boels@gmail.com>
2024-11-13 10:07:08 +0100

    corp

--- api/shareOnLinkedIn.js
+++ api/shareOnLinkedIn.js
@@ -1,4 +1,11 @@
 module.exports = async (req, res) => {
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        return res.status(204).end(); // Répond avec le statut 204 No Content
+    }
+
     if (req.method === 'POST') {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
