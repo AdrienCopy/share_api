22b25c2569012a7d7a362d34d160c804e2911dad
Adrien Boels <adrien.boels@gmail.com>
2024-11-15 14:35:10 +0100

    cors

--- api/registerUpload.js
+++ api/registerUpload.js
@@ -7,6 +7,9 @@ module.exports = async (req, res) => {
     }
 
     if (req.method === 'POST') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
         const { accessToken, fileName } = req.body;
 
         try {
